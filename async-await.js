fcf61ba7b1a73015b5d8d751cf8eceb380094c4c
dpalexandru <alexpoian91@gmail.com>
2025-11-03 12:24:52 +0530

    add daysjs

--- async-await.js
+++ async-await.js
@@ -1,6 +1,7 @@
 //In questo esercizio, utilizzerai async/await per creare la funzione getChefBirthday(id).
 //  Questa funzione accetta un id di una ricetta e deve:
 
+const dayjs = require("dayjs");
 const linkRicetta = "https://dummyjson.com/recipes/"
 const linkUsers = "https://dummyjson.com/users/"
 
@@ -18,25 +19,24 @@ async function getChefBirthday(id) {
 
   try {
     ricetta = await fetchJson(`${linkRicetta}${id}`);
-    console.log(ricetta);
     if (ricetta.message) {
       throw new Error("Id non esiste");
 
     }
   } catch (error) {
-    console.error("Errore nel recupero della ricetta:", error.message);
     throw new Error("Impossibile recuperare la ricetta richiesta");
-    return null;
   }
 
   const user = await fetchJson(`${linkUsers}${ricetta.userId}`)
-  console.log(user.birthDate)
   const birthDate = user.birthDate
   return birthDate
 
 }
 
 // uso funzione e gestione errore 
-getChefBirthday(111111111)
-  .then(birthday => console.log("Data di nascita dello chef:", birthday))
-  .catch(error => console.error("Errore:", error.message));
+getChefBirthday(4)
+  .then(birthday => {
+    // Formatto la data 
+    const formatted = dayjs(birthday).format("DD/MM/YYYY");
+    console.log("Data di nascita dello chef:", formatted);
+  }).catch(error => console.error("Errore:", error.message));
